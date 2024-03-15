be06f8eb9d2f12d590e6fa48765408d13e0d3109
mutejbhumika <132351880+mutejbhumika@users.noreply.github.com>
2024-03-15 10:22:42 +0530

    Update b1.py

--- b1.py
+++ b1.py
@@ -1,17 +1,10 @@
-# Importing the random module  
-import random  
-  
-# Creating a list  
-rand_list = []   
-  
-# Using a for loop from 0 to 9  
-for i in range(0, 10):  
-  
-    # Generates a random integer between 1 to 50   
-    n = random.randint(1, 50)  
-  
-    # Appending the ramdom integer  
-    rand_list.append(n)  
-  
-# Printing Results  
-print("List of random integers:", rand_list)  
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def hello():
+    return '\n\n\t\tBHUMIKA 21BCS7843'
+
+if _name_ == '_main_':
+    app.run(debug=True)
